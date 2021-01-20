722dbd3335c97fa99cc89d7efa933cbcf5a5b98d
anandamoyi <64667949+anandamoyi@users.noreply.github.com>
2021-01-19 19:34:12 -0500

    Fixed javadoc

--- Shopify-2021-intern-challenge/src/ImageWindowsUpload.java
+++ Shopify-2021-intern-challenge/src/ImageWindowsUpload.java
@@ -14,19 +14,11 @@ public class ImageWindowsUpload extends Application {
 
 
 	// 16d5c7e514c37b62e7d24b724551bf1ed0930e4b
-private final Group groupFour;
-	
-	public ImageWindowsUpload() {
-		groupFour = new Group();
-	}
-	
-	public Group getGroup() {
-		return groupFour;
-	}
-	
-	public static void main (String[] args) {
-		launch(args);
-	}
+/**
+xbdbchbdchbdhcb bcdcdccnhsbxgs
+cndshc hs ch sd
+nshx
+**/
 	
 	@Override
 	//creates UI
